7d81513c07f8e652c2d4ff652669559088e9896c
= <=>
2024-10-13 20:18:21 -0300

    Renamed stack

--- bin/SectorStack.ts
+++ bin/SectorStack.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { SectorStack } from '../lib/SectorStack';
 
 const app = new cdk.App();
-new SectorStack(app, 'SectorStck', {
+new SectorStack(app, 'SectorStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
